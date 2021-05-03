b576a5edfed22be1dca877755ac72c52ec267482
irvanmalik48 <irvanmalik48@gmail.com>
2021-05-03 21:49:29 +0700

    next/config: use minimize and splitChunks instead
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 const withPWA = require("next-pwa");
-const webpack = require("webpack");
 
 module.exports = withPWA({
   target: "serverless",
   webpack: function (config) {
     config.module.rules.push({ test: /\.md$/, use: "raw-loader" });
     config.module.rules.push({ test: /\.yml$/, use: "raw-loader" });
-    config.plugins = config.plugins.filter(
-      (plugin) => plugin.constructor.name !== "UglifyJsPlugin"
-    );
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin());
     return config;
   },
   compress: true,
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: "all",
+    },
+  },
   pwa: {
     dest: "public",
   },
