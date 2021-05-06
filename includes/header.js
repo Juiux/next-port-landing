d370c995a5e25ff50920e81f877264f4c5369790
irvanmalik48 <irvanmalik48@gmail.com>
2021-05-06 16:00:35 +0700

    includes: update stuffs
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

---
+++ includes/header.js
@@ -0,0 +1,36 @@
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          IrvanMA's Lair
+        </a>
+        <button
+          className="navbar-toggler p-0"
+          type="button"
+          data-mdb-toggle="collapse"
+          data-mdb-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <p className="btn btn-dark shadow-0 m-0 p-2">. . .</p>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Download CV
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
