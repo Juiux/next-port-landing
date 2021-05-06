0649dd524947f43adb80f18cf80f1d3e1b066cb2
irvanmalik48 <irvanmalik48@gmail.com>
2021-05-06 16:09:44 +0700

    dependabot: add dependabot
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- _includes/footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-dark fixed-bottom text-center text-lg-start">
-      <div className="text-center p-3">
-        © 2021 Copyright: Irvan Malik Azantha
-      </div>
-    </footer>
-  );
-}
