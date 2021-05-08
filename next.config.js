8e6715bd560d6babfae7453d49e71004b1da1357
irvanmalik48 <irvanmalik48@gmail.com>
2021-05-08 14:35:51 +0700

    next/config: workaround
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   target: "serverless",
+  externals: [/node_modules/, 'bufferutil', 'utf-8-validate'],
   future: {
     webpack5: true,
   },
