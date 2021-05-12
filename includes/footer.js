50d254e94f4f77ac5533a5a76c9ebb8b070dce14
irvanmalik48 <irvanmalik48@gmail.com>
2021-05-12 17:46:32 +0700

    footer: ...
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- includes/footer.js
+++ includes/footer.js
@@ -3,11 +3,7 @@ export default function Footer() {
     <footer className="bg-dark text-center text-lg-start">
       <div className="text-center p-3">
         Powered by <a href="https://nextjs.org/">Next.js</a> and{" "}
-        <a href="https://pages.github.com/">Github Pages</a> | Source code is
-        available at{" "}
-        <a href="https://github.com/irvanmalik48/irvanmalik48.github.io">
-          Github
-        </a>
+        <a href="https://pages.github.com/">Github</a>
         <br />
         Copyright © 2021 Irvan Malik Azantha
       </div>
