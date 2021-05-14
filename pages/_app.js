1c67b21296249bf62820785b15799574d7bccd69
irvanmalik48 <irvanmalik48@gmail.com>
2021-05-15 01:00:39 +0700

    build(deps): add mdb-ui-kit
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import "../public/styles/global.css";
+import "mdb-ui-kit/css/mdb.dark.min.css";
+
+export default function myApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
