4eb5c234e97a9ed39c03f19bb2cfc6afaf362354
irvanmalik48 <irvanmalik48@gmail.com>
2021-05-16 00:14:04 +0700

    app: import mdb-ui-kit first to prevent it from overwriting local css stylesheet
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import "../public/styles/global.css";
 import "mdb-ui-kit/css/mdb.dark.min.css";
+import "../public/styles/global.css";
 
 export default function myApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
