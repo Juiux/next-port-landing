39e364cd6b6984de424d7b3400a13fbb6bd50686
irvanmalik48 <irvanmalik48@gmail.com>
2021-05-16 20:48:34 +0700

    footer: update footer
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- includes/footer.tsx
+++ includes/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-dark text-center text-lg-start">
       <div className="text-center p-3">
         Powered by <a href="https://nextjs.org/">Next.js</a> and{" "}
-        <a href="https://pages.github.com/">Github</a>
+        <a href="https://vercel.com/">Vercel</a>
         <br />
         Copyright © 2021 Irvan Malik Azantha
       </div>
