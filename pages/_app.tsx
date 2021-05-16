0c7ea35277efa111c1ffde7c49db72197db762d4
irvanmalik48 <irvanmalik48@gmail.com>
2021-05-16 19:50:47 +0700

    styles: move global css to public dir
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "mdb-ui-kit/css/mdb.dark.min.css";
-import "./styles/global.css";
+import "../public/styles/global.css";
 
 export default function myApp({ Component, pageProps }: any) {
     return <Component {...pageProps} />;
