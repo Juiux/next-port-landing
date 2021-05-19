e2ffeee0a82c15375272e4baea3d1f785f762269
irvanmalik48 <irvanmalik48@gmail.com>
2021-05-20 03:02:44 +0700

    fix: derp
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- includes/footer.tsx
+++ includes/footer.tsx
@@ -2,8 +2,8 @@ import { GitHub, Facebook, MessageSquare, Mail, Linkedin } from "react-feather";
 
 export default function Footer() {
   return (
-    <footer className="bg-dark text-center text-lg-start">
-      <div className="text-center p-3 text-white-50">
+    <footer className="bg-dark text-center text-lg-start text-white-50">
+      <div className="text-center p-3">
         <p className="fs-1 px-2 mt-0 mb-2">
           <a aria-label="GitHub" href="https://github.com/irvanmalik48" className="text-white ripple px-3">
             <GitHub />
