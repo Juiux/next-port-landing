c69802b55a1bd8c2ac5cc5a6570ca2510e44d35e
irvanmalik48 <irvanmalik48@gmail.com>
2021-06-09 01:12:06 +0700

    fix: try to fix search bar
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- scripts/data.js
+++ scripts/data.js
@@ -20,11 +20,11 @@ function getPosts() {
 
 const fileContents = `export const posts = ${getPosts()}`;
 try {
-  fs.readdirSync("cache");
+  fs.readdirSync("public/cache");
 } catch (e) {
-  fs.mkdirSync("cache");
+  fs.mkdirSync("public/cache");
 }
-fs.writeFile("cache/data.tsx", fileContents, function (err) {
+fs.writeFile("public/cache/data.tsx", fileContents, function (err) {
   if (err) return console.log(err);
   console.log("post cached.");
 });
