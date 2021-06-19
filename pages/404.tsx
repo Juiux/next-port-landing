419c2c7d84e38325a08981f10f7851b1fd65a8f2
irvanmalik48 <irvanmalik48@gmail.com>
2021-06-19 21:51:26 +0700

    feat: add 404 page
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

---
+++ pages/404.tsx
@@ -0,0 +1,19 @@
+import DefaultLayout from "../layouts/default";
+
+export default function Index() {
+  return (
+    <DefaultLayout
+      title="404"
+      description="Page not found."
+    >
+      <section className="offline-container">
+          <p className="display-5 m-0 mb-2 text-center">
+              404
+          </p>
+          <p className="lead m-0 text-center">
+              Page not found.
+          </p>
+      </section>
+    </DefaultLayout>
+  );
+}
