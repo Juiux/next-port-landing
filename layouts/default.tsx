36c831542cb9921bb349db21870557bf1a47e429
Juiux <upw.williamscott0920@gmail.com>
2021-07-08 03:38:30 +0700

    feat: another SEO enhancement
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -11,12 +11,12 @@ export default function DefaultLayout(props: any) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <meta name="description" content={props.description} />
-        <meta name="keywords" content="lapprealm" />
+        <meta name="keywords" content="blog" />
         <meta property="og:url" content="https://irvanma.live" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={props.title} />
         <meta property="og:description" content={props.description} />
-        <meta property="og:image" content="/lp/lp.jpg" />
+        <meta property="og:image" content="https://irvanma.live/lp/lp.jpg" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="IrvanMA's Lair" />
