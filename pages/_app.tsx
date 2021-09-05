7b68da9eebcd26ba39283bf7df63775030bf224e
Juiux <upw.williamscott0920@gmail.com>
2021-09-06 02:13:18 +0700

    feat: update import path for AppProps type
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppProps } from "next/dist/next-server/lib/router/router";
+import { AppProps } from "next/dist/shared/lib/router/router";
 import "./styles/bootstrap.scss";
 import "./styles/global.scss";
 
