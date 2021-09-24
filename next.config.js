6260a10324c3e72408ccf3102db30f8e35e4c889
Juiux <upw.williamscott0920@gmail.com>
2021-09-24 15:04:51 +0700

    feat: implement preact for 35-40% bundle size reduction
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- next.config.js
+++ next.config.js
@@ -53,6 +53,16 @@ module.exports = withBundleAnalyzer(
         document: "/offline",
       },
     },
+    webpack: (config, { dev, isServer }) => {
+      if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+          react: "preact/compat",
+          "react-dom/test-utils": "preact/test-utils",
+          "react-dom": "preact/compat",
+        });
+      }
+      return config;
+    },
     async headers() {
       return [
         {
