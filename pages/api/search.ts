f003abb7f5a1a7c23323f2ff668ecd915a051991
Juiux <upw.williamscott0920@gmail.com>
2021-10-01 22:53:58 +0700

    feat: add post search API
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ pages/api/search.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSortedPostsData } from "../../api/blog";
+
+type Data = {
+  results: string[];
+};
+
+const blogPosts = getSortedPostsData();
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const results = req.query.q
+    ? blogPosts.filter((post) =>
+        post.title.toLowerCase().includes(req.query.q.toString())
+      )
+    : blogPosts;
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ results }));
+}
