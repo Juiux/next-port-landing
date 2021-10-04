fb26374aa198939011096d72148d08ba092afc67
Juiux <upw.williamscott0920@gmail.com>
2021-10-04 23:28:25 +0700

    feat: properly set twitter:image and og:image metatags
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -3,20 +3,22 @@ export function Thumbnail(
   dateP: string | Date | undefined,
   tagsP: any | string[] | undefined
 ): string {
-  let processtags = "";
-  processtags +=
+  let processedTags = "";
+  processedTags +=
     tagsP == undefined
       ? ""
       : tagsP?.map((values: string) => {
-          return `${values},`;
+          if (values != "") return `tags=${values}&`;
         });
+
+  const cleanedTags = processedTags.replaceAll(",", "");
 
   const title = `title=${titleP}&`;
   const name = `name=Irvan%20Malik%20Azantha&`;
   const email = `email=%69%72%76%61%6E%6D%61%6C%69%6B%34%38%40%67%6D%61%69%6C%2E%63%6F%6D&`;
   const date = dateP == undefined ? "" : `date=${dateP}&`;
   const color = `color=66CCCC&`;
-  const tags = tagsP == undefined ? "" : `tags=${processtags}&`;
+  const tags = tagsP == undefined ? "" : `${cleanedTags}`;
 
   const all = `https://api.irvanma.live/api/index?${title}${name}${email}${date}${color}${tags}`;
 
