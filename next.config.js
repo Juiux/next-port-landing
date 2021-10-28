b48b24244eb6f3fe62cc0fa258749ccbe9590c70
Juiux <upw.williamscott0920@gmail.com>
2021-10-28 13:00:34 +0700

    feat: remove deprecated and unused options
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 const custHeaders = [
@@ -30,58 +27,34 @@ const custHeaders = [
   },
 ];
 
-module.exports = withBundleAnalyzer(
-  withPWA({
-    target: "serverless",
-    webpack5: true,
-    compress: true,
-    reactStrictMode: true,
-    optimization: {
-      minimize: true,
-      splitChunks: {
-        chunks: "async",
-        minSize: 200,
-        maxSize: 6000,
+module.exports = withPWA({
+  compress: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+    fallbacks: {
+      document: "/offline",
+    },
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: custHeaders,
       },
-      removeAvailableModules: false,
-      usedExports: true,
-      concatenateModules: true,
-      sideEffects: true,
-      mangleExports: "size",
-      providedExports: true,
-      flagIncludedChunks: true,
-      mergeDuplicateChunks: true,
-      removeEmptyChunks: true,
-      mangleWasmImports: true,
-      nodeEnv: "production",
-      chunkIds: "deterministic",
-      moduleIds: "deterministic",
-    },
-    pwa: {
-      dest: "public",
-      disable: process.env.NODE_ENV === "development",
-      runtimeCaching,
-      fallbacks: {
-        document: "/offline",
-      },
-    },
-    webpack: (config, { dev, isServer }) => {
-      if (!dev && !isServer) {
-        Object.assign(config.resolve.alias, {
-          react: "preact/compat",
-          "react-dom/test-utils": "preact/test-utils",
-          "react-dom": "preact/compat",
-        });
-      }
-      return config;
-    },
-    async headers() {
-      return [
-        {
-          source: "/(.*)",
-          headers: custHeaders,
-        },
-      ];
-    },
-  })
-);
+    ];
+  },
+});
