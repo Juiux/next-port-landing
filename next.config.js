85b5c640f90f10386869da1850030ab213b991d3
Juiux <upw.williamscott0920@gmail.com>
2021-10-28 13:08:14 +0700

    feat: disable swc minification
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- next.config.js
+++ next.config.js
@@ -30,7 +30,6 @@ const custHeaders = [
 module.exports = withPWA({
   compress: true,
   reactStrictMode: true,
-  swcMinify: true,
   pwa: {
     dest: "public",
     disable: process.env.NODE_ENV === "development",
