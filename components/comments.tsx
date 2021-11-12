803b3b86cde923f8c78d7622a5d21d20b031b75d
Juiux <upw.williamscott0920@gmail.com>
2021-11-13 00:27:18 +0700

    feat: switch from utterances to giscus
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/comments.tsx
+++ components/comments.tsx
@@ -10,13 +10,18 @@ export default class Comments extends Component {
 
   componentDidMount(): void {
     let element: HTMLScriptElement = document.createElement("script");
-    element.setAttribute("src", "https://utteranc.es/client.js");
+    element.setAttribute("src", "https://giscus.app/client.js");
     element.setAttribute("crossorigin", "anonymous");
     element.setAttribute("async", "true");
-    element.setAttribute("repo", "irvanmalik48/blog");
-    element.setAttribute("label", "Comments");
-    element.setAttribute("issue-term", "pathname");
-    element.setAttribute("theme", "photon-dark");
+    element.setAttribute("data-repo", "irvanmalik48/blog");
+    element.setAttribute("data-repo-id", "MDEwOlJlcG9zaXRvcnkzMjQ5MTA0MTE=");
+    element.setAttribute("data-category", "Comments");
+    element.setAttribute("data-category-id", "DIC_kwDOE129S84B_1u5");
+    element.setAttribute("data-mapping", "pathname");
+    element.setAttribute("data-reactions-enabled", "0");
+    element.setAttribute("data-emit-metadata", "0");
+    element.setAttribute("data-lang", "en");
+    element.setAttribute("data-theme", "transparent_dark");
     this.box?.current?.appendChild(element);
   }
 
