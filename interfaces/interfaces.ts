d58a5ddd98d09a0f3332dc2c05c1acc747c73229
Juiux <upw.williamscott0920@gmail.com>
2021-12-01 23:41:41 +0700

    feat: wrap waves colors into an interface
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -8,3 +8,11 @@ export interface Post {
 export interface SearchProps {
   onFocusHandler: (status: boolean) => void;
 }
+
+export interface WaveAccent {
+  color1: string;
+  color2: string;
+  color3: string;
+  color4: string;
+  color5: string;
+}
