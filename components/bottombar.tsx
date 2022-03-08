2d0e21b4f611f8b09df1856972425c0adc1db911
Juiux <upw.williamscott0920@gmail.com>
2022-03-09 00:43:52 +0700

    feat: improve mobile layout's bottom bar
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/bottombar.tsx
+++ components/bottombar.tsx
@@ -1,8 +1,12 @@
 import * as Icons from "react-feather";
 import Link from "next/link";
 import { handleClick } from "./selectables";
+import { ToggleIcon } from "./navbar";
+import { useTheme } from "next-themes";
 
 export default function BottomBar() {
+  const { theme, setTheme } = useTheme();
+
   return (
     <nav className="bottombar">
       <Link href={"/"} passHref>
@@ -23,7 +27,11 @@ export default function BottomBar() {
           <p>About</p>
         </a>
       </Link>
-      <button className="stt-bottom" onClick={handleClick}>
+      <button className="bottomitems anchor" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        <ToggleIcon />
+        <p>Theme</p>
+      </button>
+      <button className="justify-self-center stt-bottom w-fit" onClick={handleClick}>
         <Icons.ChevronsUp />
       </button>
     </nav>
