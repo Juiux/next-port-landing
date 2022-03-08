1d4edafdd96f466890554f6d0ba7678f083a760f
Juiux <upw.williamscott0920@gmail.com>
2022-03-09 01:26:32 +0700

    chore: the dark ones ain't doing it yet
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,49 +7,49 @@ export default function Footer() {
         <a
           aria-label="GitHub Profile"
           href="https://github.com/irvanmalik48"
-          className="navicon shadow-none bg-transparent m-0"
+          className="navicon dark:shadow-none dark:bg-transparent shadow-none bg-transparent m-0"
         >
           <Icon.GitHub />
         </a>
         <a
           aria-label="Facebook Profile"
           href="https://facebook.com/irvanmalik48"
-          className="navicon shadow-none bg-transparent m-0"
+          className="navicon dark:shadow-none dark:bg-transparent shadow-none bg-transparent m-0"
         >
           <Icon.Facebook />
         </a>
         <a
           aria-label="Instagram Profile"
           href="https://instagram.com/irvann48_"
-          className="navicon shadow-none bg-transparent m-0"
+          className="navicon dark:shadow-none dark:bg-transparent shadow-none bg-transparent m-0"
         >
           <Icon.Instagram />
         </a>
         <a
           aria-label="Twitter Profile"
           href="https://twitter.com/irvanmalik48"
-          className="navicon shadow-none bg-transparent m-0"
+          className="navicon dark:shadow-none dark:bg-transparent shadow-none bg-transparent m-0"
         >
           <Icon.Twitter />
         </a>
         <a
           aria-label="Telegram Profile"
           href="https://t.me/irvanmalik48"
-          className="navicon shadow-none bg-transparent m-0"
+          className="navicon dark:shadow-none dark:bg-transparent shadow-none bg-transparent m-0"
         >
           <Icon.MessageSquare />
         </a>
         <a
           aria-label="LinkedIn Profile"
           href="https://www.linkedin.com/in/irvanmalik48"
-          className="navicon shadow-none bg-transparent m-0"
+          className="navicon dark:shadow-none dark:bg-transparent shadow-none bg-transparent m-0"
         >
           <Icon.Linkedin />
         </a>
         <a
           aria-label="Email Me"
           href="mailto:irvanma@gnuweeb.org"
-          className="navicon shadow-none bg-transparent m-0"
+          className="navicon dark:shadow-none dark:bg-transparent shadow-none bg-transparent m-0"
         >
           <Icon.Mail />
         </a>
