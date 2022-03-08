22872a248e2a0264536ffef763edd160fed96bff
Juiux <upw.williamscott0920@gmail.com>
2022-03-09 00:12:44 +0700

    feat: add `aria-label` property
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,28 +4,53 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="flex flex-row gap-2 justify-center items-center">
-        <a href="https://github.com/irvanmalik48" className="navicon m-0">
+        <a
+          aria-label="GitHub Profile"
+          href="https://github.com/irvanmalik48"
+          className="navicon m-0"
+        >
           <Icon.GitHub />
         </a>
-        <a href="https://facebook.com/irvanmalik48" className="navicon m-0">
+        <a
+          aria-label="Facebook Profile"
+          href="https://facebook.com/irvanmalik48"
+          className="navicon m-0"
+        >
           <Icon.Facebook />
         </a>
-        <a href="https://instagram.com/irvann48_" className="navicon m-0">
+        <a
+          aria-label="Instagram Profile"
+          href="https://instagram.com/irvann48_"
+          className="navicon m-0"
+        >
           <Icon.Instagram />
         </a>
-        <a href="https://twitter.com/irvanmalik48" className="navicon m-0">
+        <a
+          aria-label="Twitter Profile"
+          href="https://twitter.com/irvanmalik48"
+          className="navicon m-0"
+        >
           <Icon.Twitter />
         </a>
-        <a href="https://t.me/irvanmalik48" className="navicon m-0">
+        <a
+          aria-label="Telegram Profile"
+          href="https://t.me/irvanmalik48"
+          className="navicon m-0"
+        >
           <Icon.MessageSquare />
         </a>
         <a
+          aria-label="LinkedIn Profile"
           href="https://www.linkedin.com/in/irvanmalik48"
           className="navicon m-0"
         >
           <Icon.Linkedin />
         </a>
-        <a href="mailto:irvanma@gnuweeb.org" className="navicon m-0">
+        <a
+          aria-label="Email Me"
+          href="mailto:irvanma@gnuweeb.org"
+          className="navicon m-0"
+        >
           <Icon.Mail />
         </a>
       </div>
