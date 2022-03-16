ac8baa5a990dc86e356aaa9db13f0a531d8772e3
Juiux <upw.williamscott0920@gmail.com>
2022-03-17 01:17:21 +0700

    feat: add aria-label
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/bottombar.tsx
+++ components/bottombar.tsx
@@ -10,19 +10,19 @@ export default function BottomBar() {
   return (
     <nav className="bottombar">
       <Link href={"/"} passHref>
-        <a className="bottomitems" onClick={handleClick}>
+        <a className="bottomitems" onClick={handleClick} aria-label="Home">
           <Icons.Home />
           <p>Home</p>
         </a>
       </Link>
       <Link href={"/blog"} passHref>
-        <a className="bottomitems" onClick={handleClick}>
+        <a className="bottomitems" onClick={handleClick} aria-label="Blog">
           <Icons.AlignLeft />
           <p>Blog</p>
         </a>
       </Link>
       <Link href={"/about"} passHref>
-        <a className="bottomitems" onClick={handleClick}>
+        <a className="bottomitems" onClick={handleClick} aria-label="About">
           <Icons.Info />
           <p>About</p>
         </a>
@@ -30,6 +30,7 @@ export default function BottomBar() {
       <button
         className="bottomitems anchor"
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        aria-label="Toggle Theme"
       >
         <ToggleIcon />
         <p>Theme</p>
@@ -37,6 +38,7 @@ export default function BottomBar() {
       <button
         className="stt-bottom w-fit justify-self-center"
         onClick={handleClick}
+        aria-label="Go up"
       >
         <Icons.ChevronsUp />
       </button>
