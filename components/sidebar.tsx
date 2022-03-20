4af5e0026ed176b2b38999b3be0ac6dcad2d6589
Juiux <upw.williamscott0920@gmail.com>
2022-03-20 11:55:57 +0700

    feat: add shadow
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,7 +3,7 @@ import Selectables from "./selectables";
 
 export default function Sidebar(props: any) {
   return (
-    <div className={"sidebar " + props.className}>
+    <div className={"sidebar shadow-lg dark:shadow-none " + props.className}>
       <p className="sidebar-header sticky top-0 support-blur:bg-sblighttp support-blur:backdrop-blur-sm support-blur:dark:bg-sbdarktp">
         BLOG POSTS
       </p>
