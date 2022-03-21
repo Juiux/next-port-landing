2a38840f86bea423cab687d5fd2a0caf81f14ece
Juiux <upw.williamscott0920@gmail.com>
2022-03-22 00:13:49 +0700

    feat: show scroll to top only when main layer got scrolled
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/stt.tsx
+++ components/stt.tsx
@@ -1,15 +1,39 @@
 import { handleClick } from "./selectables";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import * as Icon from "react-feather";
 
 export default function Stt() {
-  return (
-    <a
-      href="#"
-      className="scroll-cursor"
-      aria-label="Back to top"
-      onClick={handleClick}
-    >
-      <Icon.ChevronsUp />
-    </a>
-  );
+  const [isVisible, setIsVisible]: [boolean, Dispatch<SetStateAction<any>>] =
+    useState(false);
+  useEffect(() => {
+    const mainPart = document.getElementById("main-part");
+    const toggleVisibility = () => {
+      if (mainPart !== null && mainPart.scrollTop > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    mainPart?.addEventListener("scroll", toggleVisibility);
+    return () => mainPart?.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  if (isVisible) {
+    return (
+      <a
+        href="#"
+        className="scroll-cursor"
+        aria-label="Back to top"
+        onClick={handleClick}
+      >
+        <Icon.ChevronsUp />
+      </a>
+    );
+  } else {
+    return (
+      <a href="#" className="scroll-cursor opacity-0" aria-label="Back to top">
+        <Icon.ChevronsUp />
+      </a>
+    );
+  }
 }
