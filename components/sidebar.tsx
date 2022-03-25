3911f7cf637458ac1dfd7b281285681a31738ca7
Juiux <upw.williamscott0920@gmail.com>
2022-03-25 22:58:48 +0700

    refactor: apply refactored style to sidebar
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,7 +4,7 @@ import Selectables from "./selectables";
 export default function Sidebar(props: any) {
   return (
     <div className={"sidebar shadow-lg dark:shadow-none " + props.className}>
-      <p className="sidebar-header sticky top-0 support-blur:bg-sblighttp support-blur:backdrop-blur-sm support-blur:dark:bg-sbdarktp">
+      <p className="sidebar-header sticky top-0 support-blur:bg-nord-light-sbtp support-blur:backdrop-blur-sm support-blur:dark:bg-nord-dark-sbtp">
         BLOG POSTS
       </p>
       <Search />
