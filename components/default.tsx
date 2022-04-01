9548871ab5c39e4e1604fb79581222fe9bd0dc04
Juiux <upw.williamscott0920@gmail.com>
2022-04-01 19:45:29 +0700

    chore: remove `preload` class script
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/default.tsx
+++ components/default.tsx
@@ -82,9 +82,6 @@ export default function Default(props: any) {
       </main>
       <BottomBar />
       <Stt />
-      <Script strategy="afterInteractive" id="stop-preload">
-        {`document.body.classList.remove("preload");`}
-      </Script>
     </>
   );
 }
