1dd742fc13f9e75572e266f387449cfb5742c010
Juiux <upw.williamscott0920@gmail.com>
2022-04-26 02:35:57 +0700

    feat: use fuzzy search
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,19 +1,29 @@
 import { cachedPosts } from "../../misc/blog";
 import { NextApiRequest, NextApiResponse } from "next";
+import Fuse from "fuse.js";
+import { json } from "stream/consumers";
 
 const blogPosts = cachedPosts;
 
 function checker(query: string) {
   query = decodeURIComponent(query).toLowerCase();
-  if (query.charAt(0) == "#") {
-    query = query.replace("#", "");
-    return blogPosts.filter((post: any) => post.tag.toString().includes(query));
-  } else if (query == "") {
+
+  let options = {
+    shouldSort: true,
+    threshold: 1,
+    location: 0,
+    fieldNormWeight: 0,
+    minMatchCharLength: 1,
+    keys: ["id", "title", "date", "desc", "tag"],
+  };
+
+  const fuse = new Fuse(cachedPosts, options);
+
+  if (query == "") {
     return undefined;
   } else {
-    return blogPosts.filter((post: any) =>
-      post.title.toLowerCase().includes(query)
-    );
+    let res = fuse.search(query);
+    return res;
   }
 }
 
