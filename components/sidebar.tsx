9c6ca2a8f242690bbc8052b8b6e333ee9dfdd02f
Juiux <upw.williamscott0920@gmail.com>
2022-04-26 02:36:54 +0700

    refactor: let search component handle it
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,4 @@
 import Search from "./search";
-import Selectables from "./selectables";
 
 export default function Sidebar(props: any) {
   return (
@@ -8,19 +7,6 @@ export default function Sidebar(props: any) {
         BLOG POSTS
       </p>
       <Search />
-      {props.allPostsData.map(
-        ({ id, title, date, tag, desc }: any, index: any) => (
-          <div className="w-full cursor-pointer" key={index}>
-            <Selectables
-              id={id}
-              title={title}
-              date={date}
-              tag={tag}
-              desc={desc}
-            />
-          </div>
-        )
-      )}
     </div>
   );
 }
